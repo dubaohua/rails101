51430a593757b22ee8ecc1f256a920edd621db4f
baohua <113983633@qq.com>
2016-12-08 22:31:47 +0800

    implement posts pagination with will_paginate

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem "will_paginate"
 gem 'bootstrap-sass'
 gem "devise"
 gem "simple_form"
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,10 +8,10 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
     @group = Group.new
     end
 
-def show
-  @group = Group.find(params[:id])
-  @posts = @group.posts.recent
-end
+    def show
+        @group = Group.find(params[:id])
+        @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
+      end
 
 def edit
 
